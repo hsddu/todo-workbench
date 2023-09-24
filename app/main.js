62d2fc2932a6b9eeaefacd9359d49d2d8b2174d7
Your Name <you@example.com>
2023-09-25 00:27:49 +0800

    fix: electron app init

---
+++ app/main.js
@@ -0,0 +1,21 @@
+// 主进程：负责控制应用的生命周期，显示原生界面，执行特殊操作并管理渲染器
+const { app, BrowserWindow } = require('electron')
+const createWindow = () => {
+    const win = new BrowserWindow({
+      width: 800,
+      height: 600
+    })
+  
+    win.loadFile('index.html')
+}
+
+app.whenReady().then(() => {
+    createWindow();
+    app.on('activate', () => {
+        if (BrowserWindow.getAllWindows().length === 0) createWindow()
+    })
+})
+
+app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') app.quit()
+})

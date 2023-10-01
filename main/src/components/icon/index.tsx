de99adcb93371acab65578f88351709540cbe36a
hsddu <2818434355@qq.com>
2023-10-01 12:59:12 +0800

    fix: create task

---
+++ main/src/components/icon/index.tsx
@@ -0,0 +1,9 @@
+export const PlusIcon = () => {
+    // iconpark：字节图标库
+    return (
+        <svg width="24" height="24" viewBox="0 0 48 48" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path d="M24.0605 10L24.0239 38" stroke="#1468f4" strokeWidth="4" strokeLinecap="round" strokeLinejoin="round"/>
+            <path d="M10 24L38 24" stroke="#1468f4" strokeWidth="4" strokeLinecap="round" strokeLinejoin="round"/>
+        </svg>
+    )
+}
--- main/src/pages/components/TaskList/index.tsx
+++ main/src/pages/components/TaskList/index.tsx
@@ -1,18 +1,31 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './index.less'
 import TaskItem from './components/TaskItem'
-import { DatePicker } from 'antd';
-import type { DatePickerProps } from 'antd';
-import locale from 'antd/es/date-picker/locale/zh_CN';
+import { DatePicker, Input } from 'antd';
+import locale from 'antd/es/date-picker/locale/zh_CN'
+import { PlusIcon } from '@/components/icon/index';
 
 export default function TaskList() {
+  const [isCreate, setIsCreate] = useState(false);
   const onChange = (date:any, dateString:string) => {
     console.log('++ onChange:', date, dateString);
   }
   return (
     <div className="task-list">
-      <h1>任务列表</h1>
-      <DatePicker locale={locale} showTime onChange={onChange} placeholder='选择任务截止日期'/>
+      <h1 className='task-list-title'>任务列表</h1>
+      <div className="add-task-container">
+        <div className="standard-container">
+          <PlusIcon></PlusIcon>
+          <Input 
+            className='add-task-container-input'
+            placeholder='创建任务' 
+            onFocus={() => setIsCreate(true)} 
+            onBlur={() => setIsCreate(false)}
+          />
+        </div>
+        { isCreate && <div>12312</div> }
+      </div>
+      {/* <DatePicker locale={locale} showTime onChange={onChange} placeholder='选择任务截止日期'/> */}
       <div className="task-item-container">
         <TaskItem title="任务一" desc="观看视频" startTime="2023-9-28" endTime='2023-9-28' status='doing' />
         <TaskItem title="任务一" desc="观看视频" startTime="2023-9-28" endTime='2023-9-28' status='doing' />
